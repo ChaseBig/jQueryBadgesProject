33824fa641faae706519891f1671145fc0f2c18b
ChaseBig <chase.small@hudl.com>
2017-08-14 12:34:50 -0500

    Pushed fix

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,9 +3,38 @@ $(function() {
   $.ajax({
     url: 'https://www.codeschool.com/users/chasesmall.json',
     dataType: 'jsonp',
-    success: function(response) {
-      // handle response
+    success: function(data) {
+    	populateWithCourses(data.courses.completed)
     }
   });
+
+   function populateWithCourses(courses) {
+
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+
+      $div = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($div);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($div);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($div);
+
+    });
+
+  }
 
 });
